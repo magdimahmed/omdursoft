46cc3ba66df47a7c7d1dbacd671e32f53d331e84
Magdi Ahmedsaid <magdi.ahmedsaid@oracle.com>
2022-01-14 16:05:35 -0800

    Fix layout part 3

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -7,6 +7,6 @@ class ContactForm(forms.Form):
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
     email_address = forms.EmailField(max_length=150)
-    Company_nale = forms.CharField(max_length=150)
+    Company_name = forms.CharField(max_length=150)
     Upload_doc = forms.FileField()
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
