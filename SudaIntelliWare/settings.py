8ddcb947bdcd9eaa8ed0af8e1c6693eb2e3ec5c4
Magdi Ahmedsaid <magdi.ahmedsaid@oracle.com>
2022-01-14 16:06:50 -0800

    Fix layout part 3

--- SudaIntelliWare/settings.py
+++ SudaIntelliWare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wqe2gp#(^(gkr2q1kln96sl0v7cr@wwnoxked5ho=ui+s3&n1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["magdi.pythonanywhere.com"]
 # "magdi.pythonanywhere.com"
 
 # Application definition
